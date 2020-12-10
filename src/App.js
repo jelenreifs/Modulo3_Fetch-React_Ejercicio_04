bf49dc4ede3b8a9e4128466c04663a22f335520e
REIFS HERNANDO <ereifs@serikat.es>
2020-12-10 20:38:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './App.css'
+
+ const Cartas = (props) => {
+   const [carta, setCarta] = useState([]);
+   
+  useEffect(() => {
+    fetch("https://api.magicthegathering.io/v1/cards")
+      .then((res) => res.json())
+      .then((res) => {
+        setCarta(res.cards);
+        console.log(res.cards)
+      });
+
+  }, [props.code]);
+   
+   console.log(props.code)
+   
+   const buscaNombre = carta.filter(item => item.set === props.code)
+   console.log(buscaNombre)
+   const mostrarCarta = buscaNombre.map(element => {
+     return (
+       <div>
+         <img src={element.imageUrl} alt={element.name} />
+         <h4 className="img-carta ">{element.imageUrl}</h4>
+         <p>{element.text}</p>
+         <p>{element.set}</p>
+       </div>
+     )
+   })  
+ 
+   return <div>{ mostrarCarta }</div>
+} 
+
 
 function App() {
+  const [data, setData] = useState([]);
+  const [code, setCode] = useState("");
+
+ useEffect(() => {
+   fetch("https://api.magicthegathering.io/v1/sets")
+     .then(res => res.json())
+       .then(res => {
+         setData(res.sets);
+       });
+ }, []);
+  
+
+    const handleChange = (e) => {
+      setCode(e.target.value);
+      console.log(e.target.value)
+    };
+  
+  const options = data.map((option, index) =>
+    <option key={index} value={option.code}>{option.name}</option >
+  ); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <select onChange={handleChange}>{options}</select>
+      <Cartas code={code}/>
+    </>
+  )
 }
 
 export default App;
